8ebbbe99a6a4b2f1248f4afaf65511bbbb596334
victoria-dube <victoriaclaire56@gmail.com>
2026-01-30 12:18:08 +0100

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using SQLitePCL;
+using stokieinventryapp.Data;
+using stokieinventryapp.Models;
+using stokieinventryapp.Views;
+using System.Windows;
+
+
+namespace stokieinventryapp
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // VERY IMPORTANT
+            Batteries.Init();
+
+            // Initialize database (creates tables)
+            var db = new DatabaseHelper();
+
+            base.OnStartup(e);
+
+            var login = new Views.login();
+            login.Show();
+        }
+    }
+}
+
+           
+    
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using stokieinventryapp.views;
+using stokieinventryapp.Views;
+using System.Windows;
+
+namespace stokieinventryapp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainContent.Content = new DashboardView1();
+        }
+
+        private void Dashboard_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new DashboardView1();
+        }
+
+        private void Inventory_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new inventory1();
+        }
+
+        private void Suppliers_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new suppliers1();
+        }
+
+        private void Orders_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new productdetails1();
+        }
+
+        private void Reports_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new reports1();
+        }
+
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new settings1();
+        }
+    }
+}
